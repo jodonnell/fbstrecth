3b25f64cb543dabad021069ceefbc87af8517b4e
Jacob O'Donnell <jacobodonnell@gmail.com>
2011-06-16 04:06:55 -0400

    capistrano is working now

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,11 +16,11 @@ role :db,  "screenmold.com", :primary => true # This is where Rails migrations w
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+#If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
